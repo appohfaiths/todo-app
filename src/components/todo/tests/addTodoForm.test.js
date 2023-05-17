01a34a6852b50004b8fd39dbff6b6485d7928734
Faith Sromawuda Appoh <appohfaiths@gmail.com>
2023-05-17 02:34:23 +0000

    wrote the logic for testing the add todos component

--- src/components/todo/tests/addTodoForm.test.js
+++ src/components/todo/tests/addTodoForm.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AddTodo from '../addTodoForm';
+import useTodoState from '@/utilities/hooks/useTodoState';
+
+jest.mock('@/utilities/hooks/useTodoState');
 
 test('add a new todo', () => {
-  render(<AddTodo />);
+  const addTodoMock = jest.fn();
+  useTodoState.mockReturnValue({ addTodo: addTodoMock });
 
-  screen.getByRole('textbox');
+  render(<AddTodo addTodo={addTodoMock} />);
+
+  const input = screen.getByTestId('todoInput');
+  const submitButton = screen.getByTestId('submitButton');
+  const newTodoText = 'testing the input';
+
+  fireEvent.change(input, { target: { value: newTodoText } });
+
+  fireEvent.click(submitButton);
+
+  expect(addTodoMock).toHaveBeenCalledWith(newTodoText);
+
+  expect(input.value).toBe('');
 });
