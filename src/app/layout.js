514978ef13baff3083ac526cd6879112a99867da
Faith Sromawuda Appoh <appohfaiths@gmail.com>
2023-05-17 02:21:15 +0000

    removed next font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
+import React from 'react';
 import './globals.css';
-import { Pontano_Sans } from 'next/font/google';
 import Header from '@/components/shared/header';
 import Footer from '@/components/shared/footer';
-
-const pontanoSans = Pontano_Sans({ subsets: ['latin'], display: 'swap' });
 
 export const metadata = {
   title: 'Simple Todo List App',
@@ -13,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${pontanoSans.className} flex flex-col min-h-screen`}>
+      <body className={`flex flex-col min-h-screen`}>
         <Header />
         {children}
         <Footer />
